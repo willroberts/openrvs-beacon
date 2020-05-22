f9782c9b55c125425e5918215a69283403b2dd18
ijemafe <will.roberts11+github2@gmail.com>
2020-05-22 11:14:08 -0700

    Update main.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"sync"
 
 	beacon "github.com/ijemafe/openrvs-beacon"
 )
@@ -16,25 +17,46 @@ func main() {
 		log.Fatal("failed to read server list:", err)
 	}
 
+	reports := make(chan *beacon.ServerReport, len(servers))
+	errs := make(chan error, len(servers))
+
+	var wg sync.WaitGroup
 	for _, s := range servers {
-		b, err := beacon.GetServerReport(s.IP, s.Port+1000)
-		if err != nil {
-			log.Println("failed to read from server:", err)
-			fmt.Println()
-			continue
-		}
+		wg.Add(1)
+		go func(s beacon.Server) {
+			b, err := beacon.GetServerReport(s.IP, s.Port+1000)
+			if err != nil {
+				errs <- err
+				wg.Done()
+				return
+			}
 
-		r, err := beacon.ParseServerReport(s.IP, b)
-		if err != nil {
-			log.Println("failed to parse report:", err)
-			fmt.Println()
-			continue
-		}
+			r, err := beacon.ParseServerReport(s.IP, b)
+			if err != nil {
+				errs <- err
+				wg.Done()
+				return
+			}
+			reports <- r
+			wg.Done()
+		}(s)
+	}
+	wg.Wait()
+	close(reports)
+	close(errs)
 
+	for e := range errs {
+		if e != nil {
+			log.Println(e)
+		}
+	}
+
+	for r := range reports {
 		fmt.Println("Server:", r.ServerName)
 		fmt.Printf("Address: %s:%d\n", r.IPAddress, r.Port)
 		fmt.Println("Game Version:", r.GameVersion)
 		fmt.Println("Mod Name:", r.ModName)
+		fmt.Println("MOTD:", r.MOTD)
 		fmt.Println("Current Map:", r.CurrentMap)
 		fmt.Println("Current Game Mode:", r.CurrentMode)
 		if r.NumTerrorists > 0 {
