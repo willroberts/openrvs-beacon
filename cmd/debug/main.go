093d2b3172224bc36183582f621b973fd6a737e7
ijemafe <noreply@ijemafe.com>
2020-06-07 13:06:26 -0700

    Extend debugger

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -1,28 +1,67 @@
+// The debug cmd retrieves UDP beacons and tests for missing data.
+// Maximum safe size for UDP packets is 512 bytes on Linux and 548 bytes on
+// Windows. OpenRVS beacons start to drop data around 1700 bytes.
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strconv"
 	"time"
 
 	beacon "github.com/ijemafe/openrvs-beacon"
 )
 
-const targetIP = "72.251.228.169"
-const targetPort = 7777
+type Input struct {
+	IP   string
+	Port int
+}
 
 func main() {
-	// test cut off game modes
-	// update: this is caused by oversized UDP beacons. a safe limit is between
-	// 512 and 548 bytes. Many beacons are over 1700 bytes, resulting in lost
-	// data.
-	b, err := beacon.GetServerReport(targetIP, targetPort+1000, 5*time.Second)
+	//targets := []Input{Input{"162.248.92.181", 7778}} // Broken beacon response
+	targets, err := getHostPorts()
 	if err != nil {
 		log.Fatal(err)
 	}
-	r, err := beacon.ParseServerReport(targetIP, b)
-	if err != nil {
-		log.Fatal(err)
+	for _, t := range targets {
+		b, err := beacon.GetServerReport(t.IP, t.Port+1000, 5*time.Second)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		_, err = beacon.ParseServerReport(t.IP, b)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 	}
-	log.Println("map count:", len(r.MapRotation))
-	log.Println("mode count:", len(r.ModeRotation))
+}
+
+func getHostPorts() ([]Input, error) {
+	var inputs = make([]Input, 0)
+	resp, err := http.Get("http://64.225.54.237/servers")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	lines := bytes.Split(bytes.TrimSuffix(b, []byte{'\n'}), []byte{'\n'})
+	for i := 1; i < len(lines); i++ {
+		fields := bytes.Split(lines[i], []byte{','})
+		host := string(fields[1])
+		portBytes := fields[2]
+		port, err := strconv.Atoi(string(portBytes))
+		if err != nil {
+			log.Println("atoi error:", err)
+			continue
+		}
+		inputs = append(inputs, Input{IP: host, Port: port})
+	}
+	return inputs, nil
 }
