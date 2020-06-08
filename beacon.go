779901cb348b2912f59820d3a65ce11bf22b0c1e
ijemafe <noreply@ijemafe.com>
2020-06-08 11:12:28 -0700

    Docs update

--- beacon.go
+++ beacon.go
@@ -1,13 +1,13 @@
-// Package beacon provides a library for interacting with a Rainbow Six 3: Raven
-// Shield game server.
+// Package beacon provides a library for interacting with Rainbow Six 3: Raven
+// Shield game servers.
 //
 // Raven Shield game servers communicate with UDP for all purposes. Servers
 // listen on UDP ports for clients, clients join games with UDP, and there is
 // communication between clients and servers over UDP as well. The format used
 // in this communication is called a "beacon". A beacon is a stream of text
 // separated by named markers containing pilcrow (¶) signs. Each segment follows
-// a specific marker, so the format is similar to a map of string keys to string
-// values.
+// a specific marker, so the format is similar to an ordered map of string keys
+// to string values.
 //
 // The UDP beacon structure looks like this (in exact order of appearance):
 // - Port (4-5 bytes)
@@ -17,10 +17,10 @@
 // - Maximum Players (2 bytes)
 // - Locked? (1 byte)
 // - Dedicated? (1 byte)
-// - Player Names (up to 20 bytes per player)
-// - Player Times (5 bytes per player)
-// - Player Pings (up to 5 bytes per player)
-// - Player Kills (up to 4 bytes per player)
+// - Player Names (0 or 20-320 bytes for 1-16 players)
+// - Player Times (0 or 5-80 bytes for 1-16 players)
+// - Player Pings (0 or 5-80 bytes for 1-16 players)
+// - Player Kills (0 or 4-64 bytes for 1-16 players)
 // - Current Players (2 bytes)
 // - Rounds Per Match (2 bytes, more above 99)
 // - Time Per Round (4 bytes, more above 2.75 hours)
@@ -46,18 +46,22 @@
 // - Mode Rotation (47-832 bytes for 1-32 modes)
 // - MOTD (up to 60 bytes, only in OpenRVS 1.5+)
 //
-// A server with one map in the rotation, no connected players, and no MOTD
-// sends around 225 bytes of data without including markers. There are 34 (or 35
-// in OpenRVS 1.5) markers each consisting of 3 bytes, for an additional 102
-// bytes (new total 327).
-//
-// With 32 maps in the rotation, 16 connected players, and a maximum-length
-// MOTD, the beacon text can reach up to 2,440 bytes in length.
+// Each component is preceded by a 3-byte marker. Overall, without the map and
+// mode rotations, and with no players connected, a server should be able to fit
+// all data within 320 bytes.
 //
 // Depending on the network and OS this code runs on, UDP data loss may occur at
 // different points. The safe limit is generally 512 bytes, and data loss could
-// occur at higher values. When this data loss occurs, game modes are trimmed
-// from the Mode Rotation (and the MOTD if present).
+// occur at higher values. In local development, data loss begins at 1024 bytes.
+// When data loss occurs, the list of game modes and the MOTD will be cut off.
+//
+// In order to avoid data loss, we may need to fragment beacons across multiple
+// UDP messages, such as moving the map rotation and/or mode rotation to
+// separate beacons. 1024 bytes is enough space to fit the base data, 8 sets of
+// connected player data, and 431 bytes of map rotation data. Some servers will
+// exceed this map rotation data length, and PVP servers will have 16 sets of
+// connected player data, so excising the game mode rotation may not be enough
+// to get responses under 1024 bytes.
 package beacon
 
 import (
