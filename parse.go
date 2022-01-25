ae3eb8de38bfef3331c495bac2ad493e172eb20c
Will Roberts <willroberts3@protonmail.com>
2022-01-24 23:53:57 -0500

    No logs, adjust docs, prevent index panics

--- parse.go
+++ parse.go
@@ -3,27 +3,25 @@ package beacon
 import (
 	"bytes"
 	"errors"
-	"log"
 	"strconv"
 	"strings"
 )
 
-const minimumLineSize = 2 // Based on key size; value can be empty.
+const keySize = 2
 
 var ErrInvalidLine = errors.New("invalid report line")
 
 func validateLine(line []byte) error {
-	if len(line) < minimumLineSize {
+	if len(line) < keySize {
 		return ErrInvalidLine
 	}
-
 	return nil
 }
 
 // Removes traling whitespace and returns the key and value from the given line.
 func lineToKeyVal(line []byte) (string, string) {
-	key := string(line[0:2])
-	val := string(bytes.Trim(line[3:], "\x20"))
+	key := string(line[0:keySize])
+	val := string(bytes.Trim(line[keySize+1:], "\x20"))
 	return key, val
 }
 
@@ -45,14 +43,13 @@ func ParseServerReport(ip string, report []byte) (*ServerReport, error) {
 		}
 
 		// Decide which action to take based on the key.
-		// Case statements can be brittle, but there's no risk of this code changing.
 		var err error
 		switch key {
 		case "A1":
 			r.MaxPlayers, err = strconv.Atoi(value)
 		case "B1":
 			r.NumPlayers, err = strconv.Atoi(value)
-		// No C1 or D1.
+		// C1 and D1 are unused.
 		case "E1":
 			r.CurrentMap = value
 		case "F1":
@@ -67,31 +64,44 @@ func ParseServerReport(ip string, report []byte) (*ServerReport, error) {
 			}
 		case "I1":
 			r.ServerName = value
-		case "J1":
-			// ModeRotation always includes 32 "/" separators, not just one for
-			// each mode. Omit empty strings from ModeRotation.
+		case "J1": // Mode Rotation.
+			// Unlike the Map Rotation, the Mode Rotation always includes 32 fields, regardless of
+			// how many actually contain values. Each field is prefixed with '/'.
 			modes := make([]string, 0)
 			fields := strings.Split(value, "/")
+			if len(fields) < 1 {
+				break
+			}
 			for _, m := range fields[1:] {
 				if m != "" {
 					modes = append(modes, m)
 				}
 			}
 			r.ModeRotation = modes
-			// Note: Mode rotation is the last thing to arrive over UDP. If it
-			// is missing any placeholder '/' characters, data loss occurred.
-			if len(fields)-1 != 32 && r.Port != 0 {
-				log.Printf("warning: data loss occurred for server %s:%d (received %d bytes)",
-					ip, r.Port, len(report))
-			}
 		case "K1":
-			r.MapRotation = strings.Split(value, "/")[1:]
+			fields := strings.Split(value, "/")
+			if len(fields) < 1 {
+				break
+			}
+			r.MapRotation = fields[1:]
 		case "L1":
-			r.ConnectedPlayerNames = strings.Split(value, "/")[1:]
+			fields := strings.Split(value, "/")
+			if len(fields) < 1 {
+				break
+			}
+			r.ConnectedPlayerNames = fields[1:]
 		case "M1":
-			r.ConnectedPlayerTimes = strings.Split(value, "/")[1:]
+			fields := strings.Split(value, "/")
+			if len(fields) < 1 {
+				break
+			}
+			r.ConnectedPlayerTimes = fields[1:]
 		case "N1":
-			in := strings.Split(value, "/")[1:]
+			in := strings.Split(value, "/")
+			if len(in) < 1 {
+				break
+			}
+			in = in[1:]
 			out := make([]int, len(in))
 			for i, l := range in {
 				var v int
@@ -103,7 +113,11 @@ func ParseServerReport(ip string, report []byte) (*ServerReport, error) {
 			}
 			r.ConnectedPlayerLatencies = out
 		case "O1":
-			in := strings.Split(value, "/")[1:]
+			in := strings.Split(value, "/")
+			if len(in) < 1 {
+				break
+			}
+			in = in[1:]
 			out := make([]int, len(in))
 			for i, l := range in {
 				var v int
@@ -124,7 +138,7 @@ func ParseServerReport(ip string, report []byte) (*ServerReport, error) {
 			r.TimeBetweenRounds, err = strconv.Atoi(value)
 		case "T1":
 			r.BombTimer, err = strconv.Atoi(value)
-		// No U1 or V1.
+		// U1 and V1 are unused.
 		case "W1":
 			if value == enabled {
 				r.TeamNamesVisible = true
@@ -179,11 +193,11 @@ func ParseServerReport(ip string, report []byte) (*ServerReport, error) {
 			if value == enabled {
 				r.PunkbusterEnabled = true
 			}
-		// O2 and above are OpenRVS custom fields.
+		// O2 through O9 are reserved for OpenRVS custom fields.
 		case "O2":
 			r.MOTD = value
 		default:
-			log.Println("unknown key:", key)
+			// Unused or invalid key.
 		}
 
 		// Several case statement branches write to err before breaking. Check it now.
